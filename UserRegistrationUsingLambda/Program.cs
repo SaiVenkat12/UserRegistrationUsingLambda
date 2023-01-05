87d1efe6c9f2992977261a9549b31c1f7d2924a0
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-05 21:47:35 +0530

    [SaiVenkat]-Validate: Validate EMail Using Lambda Expression

--- UserRegistrationUsingLambda/Program.cs
+++ UserRegistrationUsingLambda/Program.cs
@@ -8,11 +8,14 @@ namespace UserRegistrationUsingLambda
             Console.WriteLine("User Registration Using Lambda");
             Console.Write("Enter The First Name : ");
             string FName = Console.ReadLine();
-            Console.Write("Enter The Lastt Name : ");
+            Console.Write("Enter The Last Name : ");
             string LName = Console.ReadLine();
+            Console.Write("Enter the Email Address : ");
+            string EMail = Console.ReadLine();
             UserRegistration registration = new UserRegistration();
             Console.WriteLine(registration.ValidateFirstName(FName));
             Console.WriteLine(registration.ValidateLastName(LName));
+            Console.WriteLine(registration.ValidateEMail(EMail));
 
 
         }
--- UserRegistrationUsingLambda/UserRegistration.cs
+++ UserRegistrationUsingLambda/UserRegistration.cs
@@ -11,11 +11,13 @@ namespace UserRegistrationUsingLambda
     {
         public const string FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
         public const string Last_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+        public const string EMail = "^[a-zA-Z0-9]([.+-_]{0,1}[a-zA-z0-9]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,}([.]{0,1}[A-Za-z]{2,}?([.]{0,1}[A-Za-z]{2,}))*[,]{0,1}$";
         public string ValidateFirstName(string FName) =>
              Regex.IsMatch(FName, FIRST_NAME_REGEX) ? "First Name is Match" : "Name is Not a Match";
         public string ValidateLastName(string LName) =>
              Regex.IsMatch(LName, Last_NAME_REGEX) ? "Last Name Match" : "Name is Not a Match";
-
+        public string ValidateEMail(string FName) =>
+             Regex.IsMatch(FName, EMail) ? "EMail is Match" : "Email Not a Match";
 
 
     }
