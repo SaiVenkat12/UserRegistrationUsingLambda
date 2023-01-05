57c72cf0f51df5228d82c583d90dd992f7685e1e
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-05 21:28:38 +0530

    [SaiVenkat]-Validate: Validate First Name Using Lambda Expression

---
+++ UserRegistrationUsingLambda/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace UserRegistrationUsingLambda
+{
+    class program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("User Registration Using Lambda");
+            Console.Write("Enter The First Name : ");
+            string input = Console.ReadLine();
+            UserRegistration registration = new UserRegistration();
+            Console.WriteLine(registration.ValidateFirstName(input));
+           
+
+        }
+    }
+}
---
+++ UserRegistrationUsingLambda/UserRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambda
+{
+    public class UserRegistration
+    {
+        public const string FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+        public string ValidateFirstName(string input) =>
+             Regex.IsMatch(input, FIRST_NAME_REGEX) ? "Match" : "Not a Match";
+
+
+
+    }
+}
