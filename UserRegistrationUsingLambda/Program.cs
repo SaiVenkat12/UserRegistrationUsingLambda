7f7b84e07dbfd21698ddf138ad33cf1734f0b785
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-05 21:37:29 +0530

    [SaiVenkat]-Validate: Validate Last Name Using Lambda Expression

--- UserRegistrationUsingLambda/Program.cs
+++ UserRegistrationUsingLambda/Program.cs
@@ -7,10 +7,13 @@ namespace UserRegistrationUsingLambda
         {
             Console.WriteLine("User Registration Using Lambda");
             Console.Write("Enter The First Name : ");
-            string input = Console.ReadLine();
+            string FName = Console.ReadLine();
+            Console.Write("Enter The Lastt Name : ");
+            string LName = Console.ReadLine();
             UserRegistration registration = new UserRegistration();
-            Console.WriteLine(registration.ValidateFirstName(input));
-           
+            Console.WriteLine(registration.ValidateFirstName(FName));
+            Console.WriteLine(registration.ValidateLastName(LName));
+
 
         }
     }
--- UserRegistrationUsingLambda/UserRegistration.cs
+++ UserRegistrationUsingLambda/UserRegistration.cs
@@ -10,8 +10,11 @@ namespace UserRegistrationUsingLambda
     public class UserRegistration
     {
         public const string FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
-        public string ValidateFirstName(string input) =>
-             Regex.IsMatch(input, FIRST_NAME_REGEX) ? "Match" : "Not a Match";
+        public const string Last_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+        public string ValidateFirstName(string FName) =>
+             Regex.IsMatch(FName, FIRST_NAME_REGEX) ? "First Name is Match" : "Name is Not a Match";
+        public string ValidateLastName(string LName) =>
+             Regex.IsMatch(LName, Last_NAME_REGEX) ? "Last Name Match" : "Name is Not a Match";
 
 
 
