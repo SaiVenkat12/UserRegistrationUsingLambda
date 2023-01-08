2c859553117e6f4e3c1a14c3440714d51962ab3a
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-08 20:12:08 +0530

    [SaiVenkat]-Check: Checking and Should clear all email samples provided separately

--- UserRegistrationUsingLambda/Program.cs
+++ UserRegistrationUsingLambda/Program.cs
@@ -5,24 +5,30 @@ namespace UserRegistrationUsingLambda
     {
         public static void Main(string[]args)
         {
-            Console.WriteLine("User Registration Using Lambda");
-            Console.Write("Enter The First Name : ");
-            string FName = Console.ReadLine();
-            Console.Write("Enter The Last Name : ");
-            string LName = Console.ReadLine();
-            Console.Write("Enter the Email Address : ");
-            string Email = Console.ReadLine();
-            Console.Write("Enter the Mobile Number : ");
-            string MobileNo = Console.ReadLine();
-            Console.Write("Enter the Password : ");
-            string Password = Console.ReadLine();
+            //Console.WriteLine("User Registration Using Lambda");
+            //Console.Write("Enter The First Name : ");
+            //string FName = Console.ReadLine();
+            //Console.Write("Enter The Last Name : ");
+            //string LName = Console.ReadLine();
+            //Console.Write("Enter the Email Address : ");
+            //string Email = Console.ReadLine();
+            //Console.Write("Enter the Mobile Number : ");
+            //string MobileNo = Console.ReadLine();
+            //Console.Write("Enter the Password : ");
+            //string Password = Console.ReadLine();
+            string email = "Abc1@gmail.com";
+            string email2 = "abc+100@gmail.com";
+            string email3 = "abc-100@abc.net,";
 
             UserRegistration registration = new UserRegistration();
-            Console.WriteLine(registration.ValidateFirstName(FName));
-            Console.WriteLine(registration.ValidateLastName(LName));
-            Console.WriteLine(registration.ValidateEMail(Email));
-            Console.WriteLine(registration.ValidateMobileNo(MobileNo));
-            Console.WriteLine(registration.ValidatePassword(Password));
+            //Console.WriteLine(registration.ValidateFirstName(FName));
+            //Console.WriteLine(registration.ValidateLastName(LName));
+            //Console.WriteLine(registration.ValidateEMail(Email));
+            //Console.WriteLine(registration.ValidateMobileNo(MobileNo));
+            //Console.WriteLine(registration.ValidatePassword(Password));
+            Console.WriteLine(registration.ValidateEMail(email));
+            Console.WriteLine(registration.ValidateEMail(email2));
+            Console.WriteLine(registration.ValidateEMail(email3));
 
 
         }
